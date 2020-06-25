d253359c427a6d3964e175589b0950078e63b7d0
ptillemans <pti@snamellit.com>
2020-06-25 21:43:59 +0200

    working example with coded UI tree

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
+use gtk::Label;
+use gtk::Orientation::Vertical;
+use gtk::prelude::*;
+use gtk::{Window, WindowType};
+use relm::{connect, Relm, Update, Widget};
+use relm_derive::Msg;
+
+struct Model {
+    greeting: String,
+}
+
+#[derive(Msg)]
+enum Msg {
+    Quit,
+}
+
+struct Win {
+    model: Model,
+    window: Window,
+}
+
+impl Update for Win {
+    // Specify the model used for this widget.
+    type Model = Model;
+    // Specify the model parameter used to init the model.
+    type ModelParam = ();
+    // Specify the type of the messages sent to the update function.
+    type Msg = Msg;
+
+    // Return the initial model.
+    fn model(_: &Relm<Self>, _: ()) -> Model {
+        Model {
+            greeting: "Hello, World!".to_string(),
+        }
+    }
+
+    // The model may be updated when a message is received.
+    // Widgets may also be updated in this function.
+    fn update(&mut self, event: Msg) {
+        match event {
+            Msg::Quit => gtk::main_quit(),
+        }
+    }
+}
+
+impl Widget for Win {
+    // Specify the type of the root widget.
+    type Root = Window;
+
+    // Return the root widget.
+    fn root(&self) -> Self::Root {
+        self.window.clone()
+    }
+
+    // Create the widgets.
+    fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
+        // GTK+ widgets are used normally within a `Widget`.
+        let vbox = gtk::Box::new(Vertical, 0);
+        let label = Label::new(Some(&model.greeting));
+        vbox.add(&label);
+
+        let window = Window::new(WindowType::Toplevel);
+        window.set_default_size(800, 1200);
+        window.add(&vbox);
+
+        // Connect the signal `delete_event` to send the `Quit` message.
+        connect!(
+            relm,
+            window,
+            connect_delete_event(_, _),
+            return (Some(Msg::Quit), Inhibit(false))
+        );
+        // There is also a `connect!()` macro for GTK+ events that do not need a
+        // value to be returned in the callback.
+
+        window.show_all();
+
+        Win { model, window }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Win::run(()).unwrap();
 }
