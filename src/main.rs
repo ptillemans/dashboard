39a5f3d9663f08872f81f21c7cdf037634cce4c5
ptillemans <pti@snamellit.com>
2020-06-28 12:11:53 +0200

    refactored to small functionsf

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use gtk::prelude::*;
 use gtk::{Window, WindowType};
 use relm::{connect, Relm, Update, Widget};
 use relm_derive::Msg;
+use cairo;
 
 struct Model {
     greeting: String,
@@ -54,42 +55,60 @@ impl Widget for Win {
 
     // Create the widgets.
     fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
-        // GTK+ widgets are used normally within a `Widget`.
-        // First we get the file content.
-        let glade_src = include_str!("dashboard.glade");
-        // Then we call the Builder call.
-        let builder = gtk::Builder::new_from_string(glade_src);
-
-        let label: gtk::Label = builder.get_object("label1").unwrap();
-        label.set_text(&model.greeting);
-
-        let canvas: gtk::DrawingArea = builder.get_object("canvas1").unwrap();
-        canvas.connect_draw(move |_, context| {
-            context.set_source_rgb(0.2, 0.4, 0.0);
-            context.paint();
-
-            context.set_font_size(60.0);
-            context.set_source_rgb(0.0, 0.0, 0.0);
-            context.move_to(100.0, 100.0);
-            context.show_text("Hello!!!");
-            Inhibit(false)
-        });
-        let window: gtk::Window = builder.get_object("window1").unwrap();
-
-        // Connect the signal `delete_event` to send the `Quit` message.
-        connect!(
-            relm,
-            window,
-            connect_delete_event(_, _),
-            return (Some(Msg::Quit), Inhibit(false))
-        );
-        // There is also a `connect!()` macro for GTK+ events that do not need a
-        // value to be returned in the callback.
+        let builder = get_glade_builder();
+        init_label(&builder, &model);
+        init_drawing_area(&builder);
+        let window = init_window(&builder, &relm);
 
         window.show_all();
 
         Win { model, window }
     }
+}
+
+fn get_glade_builder() -> gtk::Builder {
+    // GTK+ widgets are used normally within a `Widget`.
+    // First we get the file content.
+    let glade_src = include_str!("dashboard.glade");
+    // Then we call the Builder call.
+    gtk::Builder::new_from_string(glade_src)
+
+}
+
+fn init_label(builder: &gtk::Builder, model: &Model) -> () {
+    let label: gtk::Label = builder.get_object("label1").unwrap();
+    label.set_text(&model.greeting);
+
+}
+
+fn init_drawing_area(builder: &gtk::Builder) -> (){
+    let canvas: gtk::DrawingArea = builder.get_object("canvas1").unwrap();
+    canvas.connect_draw(move |_, context| {
+        draw_drawing_area(context);
+        Inhibit(false)
+    });
+}
+
+fn init_window(builder: &gtk::Builder, relm: &Relm<Win>) -> gtk::Window {
+    let window: gtk::Window = builder.get_object("window1").unwrap();
+    // Connect the signal `delete_event` to send the `Quit` message.
+    connect!(
+        relm,
+        window,
+        connect_delete_event(_, _),
+        return (Some(Msg::Quit), Inhibit(false))
+    );
+    window
+}
+
+fn draw_drawing_area(context: &cairo::Context) -> () {
+    context.set_source_rgb(0.2, 0.4, 0.0);
+    context.paint();
+
+    context.set_font_size(60.0);
+    context.set_source_rgb(0.0, 0.0, 0.0);
+    context.move_to(100.0, 100.0);
+    context.show_text("Hello!!!");
 }
 
 fn main() {
